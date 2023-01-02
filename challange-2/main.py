74abb0013d350e0f3c70046bdc22fd3a98b9a90d
Ashish Changan <ashish.changan@ingrammicro.com>
2023-01-02 17:08:23 +0530

    update

--- challange-2/main.py
+++ challange-2/main.py
@@ -1,6 +1,15 @@
 import json
-
 from extract import json_extract
-r = json.loads(open('metadata.json').read())
-data_values = json_extract(r, 'attributes')
+fileobj = open('allMetadata1.json')
+jsonstr = fileobj.read()
+#fileobj.close()
+# do character conversion here
+outstr = jsonstr.replace('"{', '{').replace('}"', '}')
+# print the converted string
+with open('allMetadata1.json', 'w') as f:
+    f.writelines(outstr)
+fileobj.close()
+f.close()
+r = json.loads(open('allMetadata1.json').read())
+data_values = json_extract(r, 'expireOn')
 print(data_values)
--- challange-2/metadata.ps1
+++ challange-2/metadata.ps1
@@ -1,13 +1,9 @@
-﻿$allMetadata = Get-GceMetadata -Recurse -Path "instance/"
-
-$allMetadata | Out-File "C:\test\metadata.json" 
-$FilePath = "C:\test\metadata.json"
-$FileContent = Get-Content -Path $FilePath | ConvertFrom-Json
-
+﻿$allMetadata = Get-GceMetadata -Recurse -Path "instance/" | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
+$allMetadata | Out-File "C:\test\allMetadata1.json" 
+$FilePath = "C:\test\allMetadata1.json"
+$FileContent = Get-Content -Path $FilePath
 $UTF8Only = New-Object System.Text.UTF8Encoding($false)
-[System.IO.File]::WriteAllLines($FilePath, @($FileContent | ConvertTo-Json), $UTF8Only)
-
-
+[System.IO.File]::WriteAllLines($FilePath, @($FileContent), $UTF8Only)
 write-host "JSON formatted output is:" `n $allMetadata
 write-host `n`n
 #$Key = Read-Host "Please enter key to fetch value"
